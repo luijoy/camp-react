5c515ac640400239df61513f9e9c4b39533394e6
Lui Kohl <lui.kohl@gmail.com>
2018-08-13 16:21:31 +0200

    added axios fetch

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,4 +1,31 @@
 var React = require ('react');
+var PropTypes = require ('prop-types');
+var api = require ('../utils/api');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className="languages">
+      {languages.map (function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind (null, lang)}
+            key={lang}
+          >
+            {lang}
+          </li>
+        );
+      }, this)}
+    </ul>
+  );
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 class Popular extends React.Component {
   constructor (props) {
@@ -6,37 +33,43 @@ class Popular extends React.Component {
 
     this.state = {
       selectedLanguage: 'All',
+      repos: null,
     };
 
     this.updateLanguage = this.updateLanguage.bind (this);
   }
+
+  componentDidMount () {
+    this.updateLanguage (this.state.selectedLanguage);
+  }
+
   updateLanguage (lang) {
     this.setState (function () {
       return {
         selectedLanguage: lang,
+        repos: null,
       };
     });
+
+    api.fetchPopularRepos (lang).then (
+      function (repos) {
+        this.setState (function () {
+          return {
+            repos: repos,
+          };
+        });
+      }.bind (this)
+    );
   }
   render () {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
     return (
-      <ul className="languages">
-        {languages.map (function (lang) {
-          return (
-            <li
-              style={
-                lang === this.state.selectedLanguage ? {color: '#d0021b'} : null
-              }
-              onClick={this.updateLanguage.bind (null, lang)}
-              key={lang}
-            >
-              {lang}
-            </li>
-          );
-        }, this)}
-
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        {JSON.stringify (this.state.repos, null, 2)}
+      </div>
     );
   }
 }

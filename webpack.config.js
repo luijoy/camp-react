0b13742545477de01af369dcd31f7c78d5dfe920
Lui Kohl <lui.kohl@gmail.com>
2018-08-18 14:58:08 +0200

    deployed to firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require ('path');
 var HtmlWebpackPlugin = require ('html-webpack-plugin');
-module.exports = {
+var webpack = require ('webpack');
+
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve (__dirname, 'dist'),
@@ -21,5 +23,7 @@ module.exports = {
       template: 'app/index.html',
     }),
   ],
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
+
+module.exports = config;

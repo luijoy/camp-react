da5c612ddcb97a56dbf61d45d60e94745aa7f740
Lui Kohl <lui.kohl@gmail.com>
2018-08-23 11:47:35 +0200

    changed from axios to fetch

--- app/components/Results.js
+++ app/components/Results.js
@@ -43,35 +43,32 @@ Player.propTypes = {
 };
 
 class Results extends React.Component {
-  constructor (props) {
-    super (props);
-    this.state = {
-      winner: null,
-      loser: null,
-      error: null,
-      loading: true,
-    };
-  }
-  componentDidMount () {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true,
+  };
+  async componentDidMount () {
     const {playerOneName, playerTwoName} = queryString.parse (
       this.props.location.search
     );
 
-    battle ([playerOneName, playerTwoName]).then (players => {
-      if (players === null) {
-        return this.setState (() => ({
-          error: 'Looks like there was an error. Check that both users exist on Github.',
-          loading: false,
-        }));
-      }
+    const players = await battle ([playerOneName, playerTwoName]);
 
-      this.setState (() => ({
-        error: null,
-        winner: players[0],
-        loser: players[1],
+    if (players === null) {
+      return this.setState (() => ({
+        error: 'Looks like there was an error. Check that both users exist on Github.',
         loading: false,
       }));
-    });
+    }
+
+    this.setState (() => ({
+      error: null,
+      winner: players[0],
+      loser: players[1],
+      loading: false,
+    }));
   }
 
   render () {
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,17 +1,18 @@
-import axios from 'axios';
-
-function getProfile (username) {
-  return axios
-    .get (`https://api.github.com/users/${username}`)
-    .then (({data}) => data);
+async function getProfile (username) {
+  const response = await fetch (`https://api.github.com/users/${username}`);
+  return response.json ();
 }
 
-function getRepos (username) {
-  return axios.get (`https://api.github.com/users/${username}/repos`);
+async function getRepos (username) {
+  const response = await fetch (
+    `https://api.github.com/users/${username}/repos`
+  );
+
+  return response.json ();
 }
 
 function getStarCount (repos) {
-  return repos.data.reduce (
+  return repos.reduce (
     (count, {stargazers_count}) => count + stargazers_count,
     0
   );
@@ -26,29 +27,38 @@ function handleError (error) {
   return null;
 }
 
-function getUserData (player) {
-  return Promise.all ([
+async function getUserData (player) {
+  const [profile, repos] = await Promise.all ([
     getProfile (player),
     getRepos (player),
-  ]).then (([profile, repos]) => ({
+  ]);
+
+  return {
     profile,
     score: calculateScore (profile, repos),
-  }));
+  };
 }
 
 function sortPlayers (players) {
   return players.sort ((a, b => b.score - a.score));
 }
 
-export function battle (players) {
-  return Promise.all (players.map (getUserData))
-    .then (sortPlayers)
-    .catch (handleError);
+export async function battle (players) {
+  const results = await Promise.all (players.map (getUserData)).catch (
+    handleError
+  );
+
+  return results === null ? results : sortPlayers (results);
 }
 
-export function fetchPopularRepos (language) {
+export async function fetchPopularRepos (language) {
   const encodedURI = window.encodeURI (
     `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
   );
-  return axios.get (encodedURI).then (({data}) => data.items);
+
+  const response = await fetch (encodedURI).catch (handleError);
+
+  const repos = await response.json ();
+
+  return repos.items;
 }
